21310c90901e6267b46f119b70661e99f2180d10
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-10-21 15:28:39 +0200

    add table

--- javascript/populate-table-senate.js
+++ javascript/populate-table-senate.js
@@ -4,16 +4,19 @@ import {senateData} from '../data/javascript/senate-data.js';
 const table = document.getElementById("senate-data");
 const membersArr = senateData.results[0].members;
 
-
 populateTable(membersArr);
 
-
 function populateTable(element) {
+    
     for(let i = 0; i < membersArr.length; i++){
-        let {first_name, middle_name, last_name, party, state, seniority, votes_with_party_pct} = element;
-            table.insertAdjacentHTML("beforeend", "<tr><td>" + element[i].first_name +
-                " " + (element[i].middle_name ||= " ") + addUrl(element[i].url, element[i].last_name) + " "
-                + element[i].party + " " + element[i].state + " " + element[i].seniority + " " + 
+            table.insertAdjacentHTML("beforeend",
+                "<tr><td>" + element[i].first_name + " " + 
+                (element[i].middle_name ||= " ") + 
+                addUrl(element[i].url, 
+                    element[i].last_name) + " " + 
+                element[i].party + " " + 
+                element[i].state + " " + 
+                element[i].seniority + " " + 
                 element[i].votes_with_party_pct + "%" + "</td></tr>");
     }
 }
