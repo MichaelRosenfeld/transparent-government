94c49e8ba7311a8fae3965b8ea19366df6f5a414
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-03 11:05:14 +0100

    Refactor dropwdown population logic

--- javascript/populate-state-dropdown-list.js
+++ javascript/populate-state-dropdown-list.js
@@ -4,31 +4,27 @@ import {senateData} from "../data/javascript/senate-data.js";
 
 //Variables
 const stateList = document.getElementById("select-dropdown");
-let option = document.createElement("option");
 const senateArr = senateData.results[0].members;
 
 
 //Functions
-export function getStatesFromData(array) {
-    let states = [];
+export function populateStateDropdownList() {
+    let states = getStatesFromData(senateArr);
     
+    for(let i = 0; i < states.length; i++) {
+        let state = states[i];
+        let element = document.createElement("option");
+        element.textContent = state;
+        element.value = state;
+        stateList.add(element);
+    }
+}
+
+function getStatesFromData(array) {
+    let states = [];
+
     for (let i = 0; i < array.length; i++) {
         states.push(array[i].state);
     }
-    
-    let filteredStates = [...new Set(states)];
-    console.log(filteredStates);
-    return filteredStates;
-}
-
-
-export function populateStateDropdownList() {
-    let states = getStatesFromData(senateArr);
-    for(let i = 0; i < states.length; i++) {
-        let option = states[i];
-        let element = document.createElement("option");
-        element.textContent = option;
-        element.value = option;
-        stateList.add(element);
-    }
+    return [...new Set(states)];
 }
