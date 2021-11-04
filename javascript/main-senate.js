4aa0f157dcf529fb4246529c4581e783f52af2d7
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-04 14:07:35 +0100

    Removed dependency of populateStateDropdown on fixed variable. Uses Argument instead.

--- javascript/main-senate.js
+++ javascript/main-senate.js
@@ -1,8 +1,11 @@
 import {displaySelectedPartyMembers} from "./filter-members-by-party.js";
 import {populateStateDropdown} from "./populate-state-dropdown.js";
 import {displaySelectedStateMembers} from "./filter-members-by-state.js";
+import {senateData} from "../data/javascript/senate-data.js";
 
-populateStateDropdown();
+const senateArr = senateData.results[0].members;
+
+populateStateDropdown(senateArr);
 displaySelectedPartyMembers();
 displaySelectedStateMembers();
 
