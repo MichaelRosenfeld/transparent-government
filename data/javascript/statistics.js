d07154dc3ef6e41fb1c03cc678a622c5ec248dfb
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-05 17:02:31 +0100

    Save json to js object and code cleanup.

---
+++ data/javascript/statistics.js
@@ -0,0 +1,10 @@
+let statistics = {
+  "numDemocrats": 0,
+  "numRepublicans": 0,
+  "numIndependents": 0,
+  "avgVotesWithPartyBothParties": 0,
+  "leastVotedWithParty": 0,
+  "mostVotedWithParty": 0,
+  "mostMissedVotes": 0,
+  "leastMissedVotes": 0
+};
