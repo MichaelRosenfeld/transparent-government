485acd6ed36c8fd19dcba558d97cd4592dc643fa
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-08 11:25:02 +0100

    Update

---
+++ javascript/main-statistics.js
@@ -0,0 +1,16 @@
+//Imports
+import { senateData } from "../data/javascript/senate-data.js";
+import { houseData } from "../data/javascript/house-data.js";
+import { getMembers } from "./get-members.js";
+import { statistics } from "../data/javascript/statistics.js";
+
+//Variables
+const senateArr = senateData.results[0].members;
+const houseArr = houseData.results[0].members;
+const membersArr = senateArr.concat(houseArr);
+
+//Function calls
+statistics.numDemocrats = getMembers(membersArr, "D").length;
+statistics.numRepublicans = getMembers(membersArr,"R").length;
+statistics.numIndependents = getMembers(membersArr, "I").length;
+console.log(statistics);
