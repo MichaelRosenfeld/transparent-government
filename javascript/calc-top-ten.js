aef167c0e4013cf70ffbceda6d6f502389804c64
Michael Rosenfeld <michaelrosenfeld91@gmail.com>
2021-11-09 13:04:08 +0100

    Add function for least missed votes, further code cleanup

--- javascript/calc-top-ten.js
+++ javascript/calc-top-ten.js
@@ -9,21 +9,47 @@ export function calcTopTen(array, key) {
   }
 
   //Save the top ten values to this array
-  let topTen = votesArray.sort(function (a, b) { return b - a }).slice(0, 10);
-
+  let topTen = votesArray
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .slice(0, 10);
 
   //Push the top ten names associated with the top ten values to
   //the Result Array
   for (let i = 0; i < array.length; i++) {
-    array.filter(obj => {
+    array.filter((obj) => {
       if (obj[key] === topTen[i]) {
         result.push(obj.last_name);
       }
-    })
+    });
   }
 
-  //Killing duplicates
   let uniqueResult = [...new Set(result)];
+
   console.log(uniqueResult);
   return uniqueResult;
+}
+
+export function calcLeastMissedVotes(array, key) {
+  let votesArray = [];
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    votesArray.push(array[i][key]);
+  }
+
+  votesArray.sort();
+
+  for (let i = 0; i < array.length; i++) {
+    array.filter((obj) => {
+      if (result.length < 10) {
+        if (obj[key] === votesArray[i]) {
+          result.push(obj.last_name);
+        }
+      }
+    });
+  }
+  console.log(result);
+  return result;
 }
